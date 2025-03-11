6722ab19fa8f2c9a62b35227784a32f3ea2271c1
JoseMA2603 <josemartinezanguita26@gmail.com>
2025-03-11 09:00:03 +0100

    cambios botones

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-
 let velocidad = 3; // Velocidad inicial en segundos
+
 function aumentarVelocidad() {
-    velocidad = Math.max(0.5, velocidad - 0.5); // Reduce el tiempo de animación hasta un mínimo
+    velocidad = Math.max(0.5, velocidad - 0.5); // Reduce el tiempo de animación hasta un mínimo de 0.5s
+    actualizarAnimacion();
+}
+
+function reducirVelocidad() {
+    velocidad = Math.min(10, velocidad + 0.5); // Aumenta el tiempo de animación hasta un máximo de 10s
+    actualizarAnimacion();
+}
+
+function actualizarAnimacion() {
     document.getElementById('cuadrado').style.animation = `mover ${velocidad}s infinite ease-in-out`;
 }
